c8359e06dd283d0439c9757a0f146ecad30ca319
codermcq <3304812242@qq.com>
2026-07-31 21:27:31 +0800

    分类管理:新增分类功能

---
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -0,0 +1,18 @@
+package com.sky.mapper;
+
+import com.github.pagehelper.Page;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CategoryMapper {
+
+    @Insert("insert into category(type, name, sort, status, create_time, update_time, create_user, update_user) " +
+            "values (#{type},#{name},#{sort},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    void insert(Category category);
+
+}
