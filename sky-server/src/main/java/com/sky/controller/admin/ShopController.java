51fa37815459dfdded303ae8c4ff019bf13e16a2
codermcq <3304812242@qq.com>
2026-08-02 22:25:57 +0800

    店铺:修改店铺状态

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,46 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController("adminShopController")
+@RequestMapping("/admin/shop")
+@Api(tags = "店铺相关接口")
+public class ShopController {
+    private static final String KEY = "SHOP_STATUS";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 设置店铺状态
+     * @param status
+     * @return
+     */
+    @ApiOperation("设置店铺状态")
+    @PostMapping("/{status}")
+    public Result setStatus(@PathVariable Integer status) {
+        log.info("设置店铺营业状态外:{}", status == 1 ? "营业中" : "打烊中");
+        redisTemplate.opsForValue().set(KEY, status);
+        return Result.success();
+    }
+
+    /**
+     * 查询店铺状态
+     * @return
+     */
+    @ApiOperation("查询店铺状态")
+    @GetMapping("/status")
+    public Result<Integer> getStatus() {
+        Integer shopStatus = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("获取店铺状态为: {}", shopStatus == 1 ? "营业中" : "打烊中");
+        return Result.success(shopStatus);
+    }
+}
