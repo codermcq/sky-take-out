06bc7f7586dd10b7bd9e20ce8a54e168ec7793b3
codermcq <3304812242@qq.com>
2026-08-07 02:39:25 +0800

    购物车:添加,删除,清空购物车

---
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -0,0 +1,77 @@
+package com.sky.controller.user;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+import com.sky.result.Result;
+import com.sky.service.ShoppingCartService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Delete;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/user/shoppingCart")
+@Api("C端-购物车相关接口")
+public class ShoppingCartController {
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     * @return
+     */
+    @PostMapping("/add")
+    @ApiOperation("添加购物车")
+    public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO) {
+        log.info("添加购物车, 商品信息为: {}", shoppingCartDTO);
+
+        shoppingCartService.addShoppingCart(shoppingCartDTO);
+
+        return Result.success();
+    }
+
+    /**
+     * 查询购物车
+     * @return
+     */
+    @ApiOperation("查询购物车")
+    @GetMapping("/list")
+    public Result list() {
+        log.info("查询购物车");
+
+        List<ShoppingCart> list = shoppingCartService.list();
+
+        return Result.success(list);
+    }
+
+    /**
+     * 删除购物车
+     * @param shoppingCartDTO
+     * @return
+     */
+    @ApiOperation("删除购物车")
+    @PostMapping("/sub")
+    public Result sub(@RequestBody ShoppingCartDTO shoppingCartDTO) {
+        log.info("删除的购物车信息为: {}", shoppingCartDTO);
+
+        shoppingCartService.sub(shoppingCartDTO);
+
+        return Result.success();
+    }
+
+    @ApiOperation("清空购物车")
+    @DeleteMapping("/clean")
+    public Result clean() {
+        log.info("清空购物车");
+
+        shoppingCartService.clean();
+
+        return Result.success();
+    }
+}
---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,47 @@
+package com.sky.mapper;
+
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+    /**
+     * 动态查询购物车
+     * @param shoppingCart
+     * @return
+     */
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+
+    /**
+     * 添加购物车对应的菜品数量
+     * @param cart
+     */
+    @Update("update shopping_cart set number = #{number} where id = #{id}")
+    void updateNumberById(ShoppingCart cart);
+
+    /**
+     * 插入购物车
+     * @param shoppingCart
+     */
+    @Insert("insert into shopping_cart(name, image, user_id, dish_id, setmeal_id, dish_flavor, number, amount, create_time) " +
+            "VALUES(#{name},#{image},#{userId},#{dishId},#{setmealId},#{dishFlavor},#{number},#{amount},#{createTime}) ")
+    void insert(ShoppingCart shoppingCart);
+
+    /**
+     * 数量为空则在购物车中删除
+     * @param cart
+     */
+    void delete(ShoppingCart cart);
+
+    /**
+     * 清空购物车
+     * @param shoppingCart
+     */
+    @Delete("delete from shopping_cart where user_id = #{userId}")
+    void clean(ShoppingCart shoppingCart);
+}
